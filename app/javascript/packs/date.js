d11160fca79686b75bf18785ca075b423a8e2e83
Purin-Mac <s6201012620325@email.kmutnb.ac.th>
2021-11-15 04:58:45 +0700

    modify page and file

--- app/javascript/packs/date.js
+++ app/javascript/packs/date.js
@@ -1,5 +1,12 @@
+window.onload = function(){
+    var dt = new Date();
+    year = dt.getFullYear();
+    month = dt.getMonth() + 1;
+    day = dt.getDate();
+    document.getElementById('date').innerHTML=day + "/" + month + "/" + year;
+}
 var dt = new Date();
-year = dt.getFullYear();
-month = dt.getMonth() + 1;
-day = dt.getDate();
-document.getElementById('date').innerHTML=day + "/" + month + "/" + year;
+    year = dt.getFullYear();
+    month = dt.getMonth() + 1;
+    day = dt.getDate();
+    document.getElementById('date').innerHTML=day + "/" + month + "/" + year;
