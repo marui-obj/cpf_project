f6c315712f73294b3f841e426de5ee8b3fc44e56
littleprogrammer-r5r5 <s6201012630037@email.kmutnb.ac.th>
2021-11-14 22:25:24 +0700

    add basic controller methods

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,22 @@
+class DepartmentsController < ApplicationController
+
+    def new
+        @department = Department.new
+    end
+
+    def create
+        @department = Department.new(department_params)
+        if @department.save
+            continue
+        else
+            render :new
+        end
+    end
+
+    private
+    
+    def department_params
+        params.require(:department).permit(:title, :manager)
+    end
+    
+end
