20bf41baf5fac70fc7ea832a445e22ee78085f5c
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-15 09:14:03 -0500

    disable bycrypts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:username])
 
-        if !!@user && @user.authenticate(params[:password])
+        # if !!@user && @user.authenticate(params[:password])
+        if !!@user
             session[:user_id] = @user.id
             redirect_to departments_path, notice: "Welcome: " + current_user.username
         elsif !!@user && !@user.authenticate(params[:password])
