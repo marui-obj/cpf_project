8c3c11def972bafed07014c93aae6e66492e7a00
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-15 09:18:17 -0500

    fix bycrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
         if !!@user
             session[:user_id] = @user.id
             redirect_to departments_path, notice: "Welcome: " + current_user.username
-        elsif !!@user && !@user.authenticate(params[:password])
+        # elsif !!@user && !@user.authenticate(params[:password])
+        elsif !!@user
             redirect_to login_path, alert: "Incorrect username or password."
         else
             redirect_to new_user_path, alert: "No User found."
