332ed5dbd3d62fd03504daa944448daf480f3926
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-15 09:54:52 -0500

    Reversion to use bycrypts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,10 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:username])
 
-        # if !!@user && @user.authenticate(params[:password])
-        if !!@user
+        if !!@user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to departments_path, notice: "Welcome: " + current_user.username
-        # elsif !!@user && !@user.authenticate(params[:password])
-        elsif !!@user
+        elsif !!@user && !@user.authenticate(params[:password])
             redirect_to login_path, alert: "Incorrect username or password."
         else
             redirect_to new_user_path, alert: "No User found."
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }
-    # has_secure_password
+    has_secure_password
     has_one :manager
     has_one :employee
 
