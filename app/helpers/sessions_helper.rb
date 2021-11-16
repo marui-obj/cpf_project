4af7dcd70c76a1c3ae892fdc9290bff8bb39bb52
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-16 15:55:19 -0500

    Application can now specify user role from login and can now flash message when in department page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,5 +4,13 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    def is_manager?
+        logged_in? ? current_user.manager : false
+    end
+
+    def is_employee?
+        logged_in? ? current_user.employee : false
+    end
+
  
  end
