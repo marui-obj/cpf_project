cdb7733ffbd9f2833d88bba953dd775f072c0d84
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-16 22:56:01 -0500

    department page

---
+++ app/javascript/packs/date.js
@@ -0,0 +1,5 @@
+var dt = new Date();
+    year = dt.getFullYear();
+    month = dt.getMonth() + 1;
+    day = dt.getDate();
+    document.getElementById('date').innerHTML=day + "/" + month + "/" + year;
---
+++ app/javascript/packs/select.js
@@ -0,0 +1,6 @@
+document.getElementById('select-all').onclick = function() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var checkbox of checkboxes) {
+        checkbox.checked = this.checked;
+    }
+}
