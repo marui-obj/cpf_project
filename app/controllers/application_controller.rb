10c17947fbededbae581a7b44c6e45ac6130e6fb
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-17 01:07:00 -0500

    fix helper method and clear cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
+    before_action :set_cache_headers
 
     def current_user
         if session[:user_id]
@@ -11,6 +12,14 @@ class ApplicationController < ActionController::Base
 
     def authorize
         redirect_to '/login' unless self.current_user
+    end
+
+    private
+
+    def set_cache_headers
+        response.headers["Cache-Control"] = "no-cache, no-store"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
     end
 
 end
