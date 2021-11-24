cdf94f2e6231cf53805d9b521332199b1c1a63b9
marui-obj <s6201012610044@email.kmutnb.ac.th>
2021-11-24 16:44:22 -0500

    Config to deploy environment

--- app/javascript/packs/departments.js
+++ app/javascript/packs/departments.js
@@ -2,7 +2,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../js/bootstrap_js_files.js'  
 
 Rails.start()
 Turbolinks.start()
